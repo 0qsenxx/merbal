f83fe8fcab8df9aa449e8e4f6c7da8ffedab448b
0qsenxx <110487377+0qsenxx@users.noreply.github.com>
2024-05-18 09:08:42 +0000

    Deploying to gh-pages from @ 0qsenxx/merbal@0e50f81635bb0939e424de14f56a3fc2694e91c5 🚀

--- fovorites.3a8bcb00.js
+++
@@ -1,7 +0,0 @@
-!function(){let e=document.querySelector(".exercises__list");(async function(){try{let e=await fetch("https://energyflow.b.goit.study/api/filters?filter=Muscles&page=1&limit=12");return await e.json()}catch(e){console.log("err",e)}})().then(s=>{console.log(s),s.results.forEach(s=>{e.insertAdjacentHTML("afterbegin",`<li class="exercises__item">
-        <div class="exercises__wrap">
-            <h3 class="exercises-card__title">${s.name}</h3>
-            <p class="exercises-card__category">${s.filter}</p>
-        </div>
-       </li>`)})})}();
-//# sourceMappingURL=fovorites.3a8bcb00.js.map

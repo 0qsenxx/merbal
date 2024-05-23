5f922c746c9a5b67f3546db0a13222c888bbe258
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-05-23 20:38:46 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import './js/exercises';
-// import './js/footer';
+import './js/exercises';
+import './js/footer';
 import './js/exercisesAPI'
 import './js/favorites';
 import './js/exerciseModal'

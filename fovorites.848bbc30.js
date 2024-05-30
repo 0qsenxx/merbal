0e2df78cacd8065b2107924c853c598b1b32833a
0qsenxx <110487377+0qsenxx@users.noreply.github.com>
2024-05-30 12:58:26 +0000

    Deploying to gh-pages from @ 0qsenxx/merbal@c7faf4290ebc302edd90d1f932804eefd3cbd4bf 🚀

---
+++ fovorites.848bbc30.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector(".btn-open-modal"),t=document.querySelector(".btn-close-modal"),l=document.querySelector(".header-modal");e.addEventListener("click",()=>{l.style.display="block"}),t.addEventListener("click",()=>{l.style.display="none"})}();
+//# sourceMappingURL=fovorites.848bbc30.js.map
---
+++ fovorites.f43773da.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".btn-open-modal"),t=document.querySelector(".btn-close-modal"),l=document.querySelector(".header-modal");e.addEventListener("click",()=>{l.style.display="block"}),t.addEventListener("click",()=>{l.style.display="none"});
+//# sourceMappingURL=fovorites.f43773da.js.map

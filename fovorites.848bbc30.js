c389cffec8d3287e9ad6a0fad9696ddf22811337
0qsenxx <110487377+0qsenxx@users.noreply.github.com>
2024-06-01 09:46:09 +0000

    Deploying to gh-pages from @ 0qsenxx/merbal@93583087d5364adcca4be24bf8aef85b08a29b03 🚀

--- fovorites.848bbc30.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector(".btn-open-modal"),t=document.querySelector(".btn-close-modal"),l=document.querySelector(".header-modal");e.addEventListener("click",()=>{l.style.display="block"}),t.addEventListener("click",()=>{l.style.display="none"})}();
-//# sourceMappingURL=fovorites.848bbc30.js.map

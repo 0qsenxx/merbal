861f5e5c9bcb153f1b3dbc5604b1a187e60fbb0b
0qsenxx <semen.antipyuk@gmail.com>
2024-06-04 21:43:05 +0300

    add pnotify, footer validation and fix exercises

--- src/js/exerciseModal.js
+++ src/js/exerciseModal.js
@@ -1,8 +1,12 @@
 import getExercise from './exercisesAPI.js';
 import { favoritesRender } from './favorites.js';
+import { alert, defaultModules, success } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+defaultModules.set(PNotifyMobile, {});
+
 const body = document.querySelector('body');
 const openBtn = document.querySelectorAll('.body-parts-header-start-box');
-console.log(openBtn)
+console.log(openBtn);
 openBtn.forEach(el => el.addEventListener('click', onOpenModal));
 
 export default async function onOpenModal(e) {
@@ -85,7 +89,10 @@ export default async function onOpenModal(e) {
 
     if (
       evt.target.elements.ratingEmail.value.includes('@') === false ||
-      evt.target.elements.ratingEmail.value.includes('.') === false
+      evt.target.elements.ratingEmail.value.includes('.') === false ||
+      evt.target.elements.ratingEmail.value[
+        evt.target.elements.ratingEmail.value.length - 1
+      ] === '.'
     ) {
       evt.target.elements.ratingEmail.classList.add('rating-error-input');
       return;
@@ -139,7 +146,27 @@ export default async function onOpenModal(e) {
     sendReview().then(data => {
       console.log(data);
       backdropRatingRef.classList.add('is-hidden-rating');
+      success({
+        title: 'Success!',
+        text: 'Your rating has been sent successfully!',
+      });
     });
+    document
+      .querySelector('[data-1="ratingModalStars"] svg')
+      .classList.remove('rating-modal-active-star');
+    document
+      .querySelector('[data-2="ratingModalStars"] svg')
+      .classList.remove('rating-modal-active-star');
+    document
+      .querySelector('[data-3="ratingModalStars"] svg')
+      .classList.remove('rating-modal-active-star');
+    document
+      .querySelector('[data-4="ratingModalStars"] svg')
+      .classList.remove('rating-modal-active-star');
+    document
+      .querySelector('[data-5="ratingModalStars"] svg')
+      .classList.remove('rating-modal-active-star');
+    evt.target.reset();
   });
 }
 
@@ -184,16 +211,16 @@ function renderExerciseModal(
         <p class="modal-rating-amount">${Math.round(rating)}.0</p>
         <ul class="modal-rating-list">
             ${[1, 2, 3, 4, 5]
-      .map((el, i) =>
-        i < Math.round(rating)
-          ? `<li class="modal-rating-item"><svg class="modal-rating-stars gold-star"><svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+              .map((el, i) =>
+                i < Math.round(rating)
+                  ? `<li class="modal-rating-item"><svg class="modal-rating-stars gold-star"><svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
                   </svg></li>`
-          : `<li class="modal-rating-item"><svg class="modal-rating-stars grey-star"><svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  : `<li class="modal-rating-item"><svg class="modal-rating-stars grey-star"><svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#1B1B1B20"/>
                     </svg></li>`
-      )
-      .join('')}
+              )
+              .join('')}
         </ul>
     </div>
     <ul class="modal-details-list">
--- src/js/footer.js
+++ src/js/footer.js
@@ -1,30 +1,48 @@
+import { alert, defaultModules, success } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+defaultModules.set(PNotifyMobile, {});
+
 const url = `https://energyflow.b.goit.study/api/subscription`;
-const formUsers = document.querySelector(".footer__form");
+const formUsers = document.querySelector('.footer__form');
 // console.log(url)
 
 function addUsers(newUsers) {
-    return fetch(url, {
-        method: "POST",
-        body: JSON.stringify(newUsers),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-        },
-    }).then(res =>
-        res.json()
-    ).then(res =>
-        console.log(res)
-    ).catch(error =>
-        console.log(error)
-    );
-};
-
-    formUsers.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const newUsers = {
-            email: e.currentTarget.elements.email.value,
-        };
-        console.log(newUsers);
-        addUsers(newUsers);
-        e.currentTarget.reset();
-
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(newUsers),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then(res => res.json())
+    .then(data => {
+        success({
+            title: 'Success!',
+            text: data.message,
+        })
+      console.log(data);
     })
+    .catch(error => console.log(error));
+}
+
+formUsers.addEventListener('submit', e => {
+  e.preventDefault();
+  if (
+    e.target.elements.email.value.includes('@') === false ||
+    e.target.elements.email.value.includes('.') === false ||
+    e.target.elements.email.value[e.target.elements.email.value.length - 1] ===
+      '.'
+  ) {
+    e.target.elements.email.classList.add('rating-error-input');
+    return;
+  } else {
+    e.target.elements.email.classList.remove('rating-error-input');
+  }
+  const newUsers = {
+    email: e.currentTarget.elements.email.value,
+  };
+  console.log(newUsers);
+  addUsers(newUsers);
+  e.currentTarget.reset();
+});
